1db7da2d4e597b8237d731c2dd212a4c1e4f9b9a
vitche <doker.bucket@gmail.com>
2020-09-02 12:45:37 +0300

    fixed again again

--- resources/views/add-form.blade.php
+++ resources/views/add-form.blade.php
@@ -39,6 +39,7 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
     <script>
 
